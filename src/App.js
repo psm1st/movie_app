58f250f75787971e7fc72f66a5a073ee0f354469
stern <psm1st@sookmyung.ac.kr>
2024-02-13 01:40:52 +0900

    07 p.190

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import axios from 'axios';
+import Movie from './Movie';
+import'./App.css';
+class App extends React.Component {
+  state ={
+    isLoading : true,
+    movie:[],
+  };
+  getMovies =async()=>{
+    const {
+      data:{
+        data:{movies},
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({movies,isLoading:false});//movies: movies -> 앞 movies는 state, 뒤 movies는 구조분해할당으로 얻은 영화데이터 변수
+  }
+  componentDidMount(){
+    this.getMovies();
+  }
+  render(){ 
+    const { isLoading,movies } = this.state;
+    return (
+    <section class="container">
+      {isLoading ? (
+        <div class="loader">
+          <span class="loader__text">Loading...</span>
+        </div>
+        ) : (
+      <div class="movies"> 
+       {movies.map(movie=>(
+        <Movie
+        key={movie.id}
+        id={movie.id}
+        year={movie.year}
+        title={movie.title}
+        summary={movie.summary}
+        poster={movie.medium_cover_image}
+        />
+        ))}
+        </div> 
+      )}
+    </section>
+    );
+  }
 }
-
 export default App;
